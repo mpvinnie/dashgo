5d85e241abc413502642895ae7a93ba6e8e548fd
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-04-28 08:13:43 -0400

    Create dashboard flex

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,14 @@
+import { Flex } from '@chakra-ui/react'
 import { Header } from "../components/Header";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex directio="column" h="100vh">
+      <Header />
+
+      <Flex>
+
+      </Flex>
+    </Flex>
   )
 }
