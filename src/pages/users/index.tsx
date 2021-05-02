0e751a88240a55ca5bb0896ca01f632bde419e88
Vinicius Peres <vinniemalafaia@hotmail.com>
2021-05-02 09:47:24 -0400

    Create pagination feature

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   Spinner
 } from '@chakra-ui/react'
 import Link from 'next/link'
+import { useState } from 'react'
 import { RiAddLine } from 'react-icons/ri'
 
 import { Header } from '../../components/Header'
@@ -24,7 +25,8 @@ import { Sidebar } from '../../components/Sidebar'
 import { useUsers } from '../../services/hooks/useUsers'
 
 export default function UserList(): JSX.Element {
-  const { data, isLoading, isFetching, error } = useUsers()
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page)
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -81,7 +83,7 @@ export default function UserList(): JSX.Element {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => (
+                  {data.users.map((user) => (
                     <Tr key={user.id}>
                       <Td px={['4', '4', '6']}>
                         <Checkbox colorScheme="pink" />
@@ -99,7 +101,11 @@ export default function UserList(): JSX.Element {
                   ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
